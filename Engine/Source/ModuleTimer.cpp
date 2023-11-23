be30d60017d075135b4240cfd5fda5fd0f0aca51
JordiAlonsoLlovet <jordial.alonso.llovet@gmail.com>
2023-11-23 19:45:38 +0100

    precise timer

--- Engine/Source/ModuleTimer.cpp
+++ Engine/Source/ModuleTimer.cpp
@@ -12,19 +12,38 @@ ModuleTimer::~ModuleTimer() {}
 bool ModuleTimer::Init() {
 	minDelta = 1000 / (long)fpsLimit;
 	ptime = std::clock();
+	
 	longTime = std::clock();
 	return true;
 }
 update_status ModuleTimer::Update() {
 	++frameCounter;
+	static Uint64 LastTime = SDL_GetPerformanceCounter();
+	static Uint64 frequency = SDL_GetPerformanceFrequency();
+	static double minElaps = frequency / fpsLimit;
+	Uint64 ncounter = SDL_GetPerformanceCounter();
+	Uint64 ElapsedTime = (ncounter - LastTime);
+
 	deltaTime = (clock() - ptime);
+	/*if (ElapsedTime < minElaps)
+	{
+		SDL_Delay((minElaps - ElapsedTime)*CLOCKS_PER_SEC/frequency);
+		ncounter = SDL_GetPerformanceCounter();
+		ElapsedTime = (ncounter - LastTime);
+	}*/
+
 	if (deltaTime < minDelta)
 	{
 		SDL_Delay(minDelta - deltaTime);
 		deltaTime = (clock() - ptime);
 	}
-	
+
 	ptime = clock();
+	LastTime = ncounter;
+
+	//double MSPerFrame = (((1000.0f * (double)ElapsedTime) / (double)frequency));
+	//double FPS = (double)frequency / (double)ElapsedTime;
+
 	if (frameCounter >= 100) {
 		long c = clock() - longTime;
 		fps = frameCounter * CLOCKS_PER_SEC / c;
